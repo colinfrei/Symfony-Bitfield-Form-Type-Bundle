2350df09997b2cc3c42bd8fa97dde5d619841afd
Dorian Villet <gnutix@gmail.com>
2015-02-27 16:46:36 +0100

    Finish updating BitfieldType for Symfony 2.1.

--- Form/Type/BitfieldType.php
+++ Form/Type/BitfieldType.php
@@ -5,29 +5,42 @@ namespace ColinFrei\BitFieldTypeBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use ColinFrei\BitFieldTypeBundle\Form\DataTransformer\BitfieldToArrayTransformer;
 
 class BitfieldType extends AbstractType
 {
-    public function getDefaultOptions(array $options)
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        return array(
+        $resolver->setDefaults(array(
             'expanded' => true,
             'multiple' => true,
-        );
+        ));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->addModelTransformer(new BitfieldToArrayTransformer());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getParent()
     {
         return new ChoiceType();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getName()
     {
         return 'bitfield';
