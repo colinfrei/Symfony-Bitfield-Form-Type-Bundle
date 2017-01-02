6ec83a43130f895da26666cfb2e1b5d568a9ad05
Christopher Gross <christopher.gross@xitaso.com>
2017-01-02 17:16:16 +0100

    rm obsolete use statement

--- Form/Type/BitfieldType.php
+++ Form/Type/BitfieldType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use ColinFrei\BitFieldTypeBundle\Form\DataTransformer\BitfieldToArrayTransformer;
 
